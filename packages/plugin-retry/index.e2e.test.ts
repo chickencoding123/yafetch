82952a612f712dc700acd941bbe1e4afde42278a
chickencoding123 <18017298+chickencoding123@users.noreply.github.com>
2023-01-24 21:14:54 -0500

    test: improving tests

--- packages/plugin-retry/index.e2e.test.ts
+++ packages/plugin-retry/index.e2e.test.ts
@@ -1,12 +1,13 @@
-import { createServer } from 'http'
+import { createServer, RequestListener } from 'http'
 import { AddressInfo } from 'net'
 import axios, { Method } from 'axios'
 import Yafetch from '@yafetch/core'
 import AbortController from 'abort-controller'
-import YafetchPluginRetry from './index'
+import YafetchPluginRetry, { YafetchPluginRetryOptions } from './index'
 
 jest.setTimeout(60000)
 
+/** Helper to make API call in node environment. Uses "axios", but can be any API library. */
 const createProxy = (validateStatus: boolean) => ({
   fetch: async (input: RequestInfo, init?: RequestInit | undefined): Promise<Response> => {
     const response = await axios({
@@ -23,200 +24,191 @@ const createProxy = (validateStatus: boolean) => ({
       type: 'default',
       url: response.config.url,
       text: () => Promise.resolve(response.data)
-    } as any)
+    } as unknown as Response)
   }
 })
+
+/** Helper function to run an HTTP server */
+const withServer = (serverCallback: RequestListener, clientCallback: (port: number) => Promise<void>) => {
+  const server = createServer(serverCallback)
+
+  return new Promise<void>((resolve, reject) => {
+    server.listen(async () => {
+      const { port } = server.address() as AddressInfo
+      try {
+        await clientCallback(port)
+        resolve()
+      } catch (err) {
+        reject(err)
+      } finally {
+        server.close()
+      }
+    })
+    server.on('error', reject)
+  })
+}
 
 describe('@yafetc/plugin-retry integration tests', () => {
   it('retries when response is a 500 error', () => {
     let i = 0
-    const server = createServer((req, res) => {
-      if (i++ < 2) {
-        res.writeHead(500)
-        res.end()
-      } else {
-        res.end('test')
-      }
-    })
-
-    return new Promise<void>((resolve, reject) => {
-      server.listen(async () => {
-        try {
-          const { port } = server.address() as AddressInfo
-          const res = await Yafetch(`http://localhost:${port}`, {
-            fetchProxy: createProxy(true).fetch,
-            plugins: {
-              wrap: [
-                YafetchPluginRetry({
-                  delay: 2
-                })
-              ]
-            }
-          })
-          expect(await res.text()).toBe('test')
-          resolve()
-        } catch (err) {
-          reject(err)
-        } finally {
-          server.close()
+    return withServer(
+      (req, res) => {
+        if (i++ < 2) {
+          res.writeHead(500)
+          res.end()
+        } else {
+          res.end('test')
         }
-      })
-      server.on('error', reject)
-    })
+      },
+      async (port) => {
+        const res = await Yafetch(`http://localhost:${port}`, {
+          fetchProxy: createProxy(true).fetch,
+          plugins: {
+            wrap: [
+              YafetchPluginRetry({
+                delay: 2
+              })
+            ]
+          }
+        })
+        expect(await res.text()).toBe('test')
+      }
+    )
   })
 
   it('resolves on >MAX_RETRIES', () => {
-    const server = createServer((req, res) => {
-      res.writeHead(500)
-      res.end()
-    })
+    return withServer(
+      (req, res) => {
+        res.writeHead(500)
+        res.end()
+      },
+      async (port) => {
+        const res = await Yafetch(`http://localhost:${port}`, {
+          fetchProxy: createProxy(true).fetch,
+          plugins: {
+            wrap: [
+              YafetchPluginRetry({
+                delay: 2,
+                maxRetries: 3
+              })
+            ]
+          }
+        })
 
-    return new Promise<void>((resolve, reject) => {
-      server.listen(async () => {
-        try {
-          const { port } = server.address() as AddressInfo
-          const res = await Yafetch(`http://localhost:${port}`, {
-            fetchProxy: createProxy(true).fetch,
-            plugins: {
-              wrap: [
-                YafetchPluginRetry({
-                  delay: 2,
-                  maxRetries: 3
-                })
-              ]
-            }
-          })
-
-          expect(res.status).toBe(500)
-          return resolve()
-        } finally {
-          server.close()
-        }
-      })
-      server.on('error', reject)
-    })
+        expect(res.status).toBe(500)
+      }
+    )
   })
 
   it('accepts a custom onRetry option', () => {
-    const server = createServer((req, res) => {
-      res.writeHead(500)
-      res.end()
-    })
+    const opts = {
+      delay: 2,
+      onRetry: jest.fn<boolean, [number, Error | undefined, Response | undefined]>(() => true),
+      maxRetries: 3
+    }
 
-    return new Promise<void>((resolve, reject) => {
-      const opts = {
-        delay: 2,
-        onRetry: jest.fn<boolean, [number, Error | undefined, Response | undefined]>(() => true),
-        maxRetries: 3
+    return withServer(
+      (req, res) => {
+        res.writeHead(500)
+        res.end()
+      },
+      async (port) => {
+        const res = await Yafetch(`http://localhost:${port}`, {
+          fetchProxy: createProxy(true).fetch,
+          plugins: {
+            wrap: [
+              YafetchPluginRetry(opts)
+            ]
+          }
+        })
+        expect(opts.onRetry.mock.calls.length).toBe(3)
+        expect(opts.onRetry.mock.calls[0][2]?.status).toBe(500)
+        expect(opts.onRetry.mock.calls[1][2]?.status).toBe(500)
+        expect(opts.onRetry.mock.calls[2][2]?.status).toBe(500)
+        expect(res.status).toBe(500)
       }
+    )
+  })
 
-      server.listen(async () => {
+  it('handles the "Retry-After" header', () => {
+    return withServer(
+      (req, res) => {
+        res.writeHead(429, { 'Retry-After': 1 })
+        res.end()
+      },
+      async (port) => {
+        const startedAt = Date.now()
+        await Yafetch(`http://localhost:${port}`, {
+          fetchProxy: createProxy(true).fetch,
+          plugins: {
+            wrap: [
+              YafetchPluginRetry({
+                delay: 4
+              })
+            ]
+          }
+        })
+        expect(Date.now() - startedAt).toBeGreaterThanOrEqual(1010)
+      }
+    )
+  })
+
+  it('stops retrying when fetch throws `ERR_UNESCAPED_CHARACTERS` error', () => {
+    const opts = {
+      onRetry: jest.fn<boolean, [number, Error | undefined, Response | undefined]>(() => true),
+      delay: 3
+    }
+
+    return withServer(
+      (req, res) => {
+        res.writeHead(200)
+        res.end()
+      },
+      async (port) => {
+        let err: Error | undefined
         try {
-          const { port } = server.address() as AddressInfo
-          const res = await Yafetch(`http://localhost:${port}`, {
-            fetchProxy: createProxy(true).fetch,
+          await Yafetch(`http://localhost:${port}/\u0019`, {
+            fetchProxy: () => {
+              throw Error('Request path contains unescaped characters')
+            },
             plugins: {
               wrap: [
                 YafetchPluginRetry(opts)
               ]
             }
           })
-          expect(opts.onRetry.mock.calls.length).toBe(3)
-          expect(opts.onRetry.mock.calls[0][2]?.status).toBe(500)
-          expect(opts.onRetry.mock.calls[1][2]?.status).toBe(500)
-          expect(opts.onRetry.mock.calls[2][2]?.status).toBe(500)
-          expect(res.status).toBe(500)
-          return resolve()
-        } finally {
-          server.close()
+        } catch (_err) {
+          err = _err as Error
         }
-      })
-      server.on('error', reject)
-    })
+
+        expect(err).not.toBeUndefined()
+        expect(err?.message).toBe('Request path contains unescaped characters')
+        expect(opts.onRetry.mock.calls.length).toBe(0)
+      }
+    )
   })
 
-  it('handles the "Retry-After" header', async () => {
-    const server = createServer((req, res) => {
-      res.writeHead(429, { 'Retry-After': 1 })
-      res.end()
-    })
-
-    return new Promise<void>((resolve, reject) => {
-      server.listen(async () => {
-        const { port } = server.address() as AddressInfo
-        try {
-          const startedAt = Date.now()
-          await Yafetch(`http://localhost:${port}`, {
-            fetchProxy: createProxy(true).fetch,
-            plugins: {
-              wrap: [
-                YafetchPluginRetry({
-                  delay: 4
-                })
-              ]
-            }
-          })
-          expect(Date.now() - startedAt).toBeGreaterThanOrEqual(1010)
-          resolve()
-        } catch (err) {
-          reject(err)
-        } finally {
-          server.close()
-        }
-      })
-      server.on('error', reject)
-    })
-  })
-
-  it('stops retrying when fetch throws `ERR_UNESCAPED_CHARACTERS` error', async () => {
-    const opts = {
-      onRetry: jest.fn<boolean, [number, Error | undefined, Response | undefined]>(() => true),
-      delay: 3
-    }
-
-    let err: Error | undefined
-    try {
-      await Yafetch('http://localhost/\u0019', {
-        fetchProxy: createProxy(true).fetch,
-        plugins: {
-          wrap: [
-            YafetchPluginRetry(opts)
-          ]
-        }
-      })
-    } catch (_err) {
-      err = _err as Error
-    }
-
-    expect(err).not.toBeUndefined()
-    expect(err?.message).toBe('Request path contains unescaped characters')
-    expect(opts.onRetry.mock.calls.length).toBe(0)
-  })
-
-  it('does not retry if the request was aborted', async () => {
+  it('does not retry if the request was aborted', () => {
     const timeout = 50
     const responseAfter = 100
-    const server = createServer((req, res) => {
-      setTimeout(() => {
-        res.end('ha')
-      }, responseAfter)
-    })
-
     const controller = new AbortController()
-    const timeoutHandler = setTimeout(() => {
-      controller.abort()
-    }, timeout)
     const opts = {
       onRetry: jest.fn<boolean, [number, Error | undefined, Response | undefined]>(() => true)
     }
 
-    return new Promise<void>((resolve, reject) => {
-      server.listen(async () => {
+    setTimeout(() => controller.abort(), timeout)
+
+    return withServer(
+      (req, res) => {
+        setTimeout(() => {
+          res.end('ha')
+        }, responseAfter)
+      },
+      async (port) => {
         try {
-          const { port } = server.address()as AddressInfo
           await Yafetch(`http://localhost:${port}`, {
             fetchProxy: createProxy(true).fetch,
-            signal: controller.signal,
+            signal: controller.signal as AbortSignal,
             plugins: {
               wrap: [
                 YafetchPluginRetry({
@@ -225,59 +217,56 @@ describe('@yafetc/plugin-retry integration tests', () => {
               ]
             }
           })
-          resolve()
         } catch (err) {
           expect(opts.onRetry.mock.calls.length).toBe(0)
-          resolve()
-        } finally {
-          server.close()
-          clearTimeout(timeoutHandler)
         }
-      })
-      server.on('error', reject)
-    })
+      }
+    )
   })
 
-  it('awaits an "onRetry" promise', async () => {
-    const server = createServer((req, res) => {
-      res.writeHead(500)
-      res.end()
-    })
-
-    const opts = {
-      onRetry: jest.fn(() => {
-        return new Promise<boolean>((resolve) => {
-          setTimeout(() => {
-            resolve(false)
-          }, 10000)
-        })
-      }),
-      delay: 4
-    }
-
-    return new Promise<void>((resolve, reject) => {
-      server.listen(async () => {
-        const { port } = server.address() as AddressInfo
-        try {
-          const startedAt = Date.now()
-          await Yafetch(`http://localhost:${port}`, {
-            fetchProxy: createProxy(true).fetch,
-            plugins: {
-              wrap: [
-                YafetchPluginRetry(opts)
-              ]
-            }
-          })
-          expect(Date.now() - startedAt).toBeGreaterThanOrEqual(10000)
-          expect(opts.onRetry.mock.calls.length).toBe(1)
-          resolve()
-        } catch (err) {
-          reject(err)
-        } finally {
-          server.close()
+  it('can handle multiple retries with custom "onRetry" logic which needs waiting every time', () => {
+    let i = 0
+    let totalMilliseconds = 0
+    const dependeeFn = () => {
+      return new Promise<boolean>((resolve) => {
+        ++i
+        if (i < 4) {
+          totalMilliseconds += 500
+          setTimeout(() => resolve(true), 500)
+        } else {
+          resolve(false)
         }
       })
-      server.on('error', reject)
-    })
+    }
+    const dependantFn = async () => {
+      totalMilliseconds += 2000
+      const result = await dependeeFn()
+      return result
+    }
+    const opts: YafetchPluginRetryOptions & { onRetry: jest.Mock<Promise<boolean>> } = {
+      onRetry: jest.fn(dependantFn),
+      delay: 2,
+      maxRetries: 4,
+      mode: 'manual'
+    }
+
+    return withServer(
+      (req, res) => {
+        res.writeHead(500)
+        res.end()
+      },
+      async (port) => {
+        await Yafetch(`http://localhost:${port}`, {
+          fetchProxy: createProxy(true).fetch,
+          plugins: {
+            wrap: [
+              YafetchPluginRetry(opts)
+            ]
+          }
+        })
+        expect(totalMilliseconds).toEqual(9500)
+        expect(opts.onRetry.mock.calls.length).toBe(4)
+      }
+    )
   })
 })
